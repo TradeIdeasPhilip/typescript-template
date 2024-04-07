b55bb45aa778db802ca6323caccfefa7c0c9793a
Philip Smolen <philip@trade-ideas.com>
2024-04-06 23:14:10 -0700

    First release candidate.
    Removed unnecessary stuff.
    Added some simple samples / test code.

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,9 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// This is the preferred way to include a css file.
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+import { getById } from "phil-lib/client-misc";
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// getById() is usually the first function I import.
+// This is the best way to access HTML from TypeScript most of the time.
+const typescriptStatusSpan = getById("typescriptStatus", HTMLSpanElement);
+typescriptStatusSpan.innerText = "working";
